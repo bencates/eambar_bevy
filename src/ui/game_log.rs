ba1e6e56c376692733a6190626de0bef522c712e
Ben Cates <ben@mojotech.com>
2023-10-11 13:29:05 -0600

    draw game log

--- src/ui/game_log.rs
+++ src/ui/game_log.rs
@@ -1,4 +1,5 @@
 use {
+    super::UI,
     crate::{
         character::Name,
         level::{CompassDirection, Position},
@@ -6,29 +7,74 @@ use {
     bevy::prelude::*,
 };
 
-#[derive(Default, Resource)]
-pub struct GameLog(Vec<String>);
+#[derive(Component)]
+pub struct LogWindowNode;
 
 #[derive(Event)]
 pub enum LogEvent {
     Move(Entity, CompassDirection, Position),
 }
 
+pub(super) fn draw_log_window(ui: Res<UI>, mut commands: Commands) {
+    commands.entity(ui.log_frame).with_children(|frame| {
+        frame
+            .spawn(NodeBundle {
+                style: Style {
+                    flex_direction: FlexDirection::Column,
+                    overflow: Overflow::clip_y(),
+                    ..default()
+                },
+                ..default()
+            })
+            .with_children(|scroll_container| {
+                scroll_container.spawn((
+                    LogWindowNode,
+                    NodeBundle {
+                        style: Style {
+                            flex_direction: FlexDirection::Column,
+                            ..default()
+                        },
+                        ..default()
+                    },
+                ));
+            });
+    });
+}
+
 pub(super) fn collect_log_events(
-    mut log: ResMut<GameLog>,
+    mut commands: Commands,
     mut log_events: EventReader<LogEvent>,
+    log_window_query: Query<Entity, With<LogWindowNode>>,
     names: Query<&Name>,
 ) {
-    for event in log_events.iter() {
-        match event {
-            &LogEvent::Move(entity, dir, pos) => {
-                if let Ok(Name(name)) = names.get(entity) {
-                    let log_entry = format!("{name} moved {dir}");
-                    log.0.push(log_entry);
+    let mut log_window = commands.entity(log_window_query.single());
 
-                    debug!("{name} moved {dir} to {pos}");
-                }
-            }
+    for event in log_events.iter() {
+        let log_entry = match event {
+            LogEvent::Move(entity, dir, _pos) => names
+                .get(*entity)
+                .ok()
+                .map(|Name(name)| format!("{name} moved {dir}")),
+        };
+
+        if let Some(log_entry) = log_entry {
+            log_window.with_children(|parent| {
+                parent.spawn(TextBundle::from_section(log_entry, TextStyle::default()));
+            });
         }
     }
+}
+
+pub(super) fn scroll_log_window(
+    mut query: Query<(&Parent, &Node, &mut Style), With<LogWindowNode>>,
+    nodes: Query<&Node>,
+) {
+    let (container, log, mut style) = query.single_mut();
+
+    let log_height = log.size().y;
+    let container_height = nodes.get(container.get()).unwrap().size().y;
+
+    let offset = (log_height - container_height).max(0.);
+
+    style.top = Val::Px(-offset);
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -4,15 +4,51 @@ pub use game_log::LogEvent;
 
 use {
     bevy::prelude::*,
-    game_log::{collect_log_events, GameLog},
+    game_log::{collect_log_events, draw_log_window, scroll_log_window},
 };
 
 pub struct UIPlugin;
 
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<GameLog>()
-            .add_event::<LogEvent>()
-            .add_systems(PostUpdate, collect_log_events);
+        app.add_event::<LogEvent>()
+            .init_resource::<UI>()
+            .add_systems(Startup, draw_log_window)
+            .add_systems(PostUpdate, (collect_log_events, scroll_log_window).chain());
+    }
+}
+
+#[derive(Resource)]
+pub struct UI {
+    log_frame: Entity,
+}
+
+impl FromWorld for UI {
+    fn from_world(world: &mut World) -> Self {
+        let log_frame = world
+            .spawn(NodeBundle {
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    right: Val::Px(0.),
+                    bottom: Val::Px(0.),
+
+                    width: Val::Vw(30.),
+                    height: Val::Px(200.),
+                    border: UiRect {
+                        top: Val::Px(3.),
+                        left: Val::Px(3.),
+                        ..default()
+                    },
+                    padding: UiRect::all(Val::Px(6.)),
+
+                    ..default()
+                },
+                background_color: Color::BLACK.into(),
+                border_color: Color::WHITE.into(),
+                ..default()
+            })
+            .id();
+
+        UI { log_frame }
     }
 }
