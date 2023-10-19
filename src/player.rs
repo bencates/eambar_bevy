6a64c011ff648da694651711b69dbb5267305d3e
Ben Cates <ben@mojotech.com>
2023-10-19 16:09:55 -0600

    basic targeting

--- src/player.rs
+++ src/player.rs
@@ -4,13 +4,15 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_event::<AttackOrMoveEvent>().add_systems(
-            Update,
-            (keyboard_input, attack_or_move)
-                .chain()
-                .in_set(PlanTurn)
-                .run_if(player_has_initiative),
-        );
+        app.add_event::<AttackOrMoveEvent>()
+            .add_event::<TargetEvent>()
+            .add_systems(
+                Update,
+                (keyboard_input, attack_or_move, set_target)
+                    .chain()
+                    .in_set(PlanTurn)
+                    .run_if(player_has_initiative),
+            );
     }
 }
 
@@ -21,9 +23,13 @@ impl Player {
     const Z_INDEX: f32 = 10.;
 }
 
+#[derive(Component)]
+pub struct Target(Option<Entity>);
+
 #[derive(Bundle)]
 pub struct PlayerBundle {
     marker: Player,
+    target: Target,
     character: CharacterBundle,
 }
 
@@ -31,6 +37,7 @@ impl PlayerBundle {
     pub fn new(text_sprite: &TextSprite) -> PlayerBundle {
         PlayerBundle {
             marker: Player,
+            target: Target(None),
             character: CharacterBundle {
                 marker: Character::Player,
                 name: Name::new("Player"),
@@ -48,6 +55,13 @@ impl PlayerBundle {
 #[derive(Event)]
 struct AttackOrMoveEvent(CompassDirection);
 
+#[derive(Event)]
+enum TargetEvent {
+    Next,
+    Prev,
+    Set(Entity),
+}
+
 fn player_has_initiative(query: Query<(&Player, &HasInitiative)>) -> bool {
     !query.is_empty()
 }
@@ -55,6 +69,7 @@ fn player_has_initiative(query: Query<(&Player, &HasInitiative)>) -> bool {
 fn keyboard_input(
     keys: Res<Input<KeyCode>>,
     mut attack_or_move_events: EventWriter<AttackOrMoveEvent>,
+    mut target_events: EventWriter<TargetEvent>,
 ) {
     if keys.just_pressed(KeyCode::Q) {
         attack_or_move_events.send(AttackOrMoveEvent(NorthWest));
@@ -74,6 +89,14 @@ fn keyboard_input(
     if keys.just_pressed(KeyCode::D) {
         attack_or_move_events.send(AttackOrMoveEvent(SouthEast));
     }
+
+    if keys.just_pressed(KeyCode::Tab) {
+        if keys.any_pressed([KeyCode::ShiftLeft, KeyCode::ShiftRight]) {
+            target_events.send(TargetEvent::Prev);
+        } else {
+            target_events.send(TargetEvent::Next);
+        }
+    }
 }
 
 #[allow(clippy::type_complexity)]
@@ -81,6 +104,7 @@ fn attack_or_move(
     mut attack_or_move_events: EventReader<AttackOrMoveEvent>,
     player_query: Query<(Entity, &Position), With<Player>>,
     targets: Query<(Entity, &Position), (With<Character>, Without<Player>)>,
+    mut target_events: EventWriter<TargetEvent>,
     mut melee_events: EventWriter<MeleeEvent>,
     mut move_events: EventWriter<MoveEvent>,
 ) {
@@ -90,8 +114,52 @@ fn attack_or_move(
         let new_pos = player_pos + dir;
 
         match targets.iter().find(|(_, &pos)| pos == new_pos) {
-            Some((target_id, _)) => melee_events.send(MeleeEvent(player_id, target_id)),
+            Some((target_id, _)) => {
+                target_events.send(TargetEvent::Set(target_id));
+                melee_events.send(MeleeEvent(player_id, target_id))
+            }
             None => move_events.send(MoveEvent(player_id, new_pos)),
         }
+    }
+}
+
+#[allow(clippy::type_complexity)]
+fn set_target(
+    mut player_query: Query<(&Viewshed, &mut Target), With<Player>>,
+    targets_query: Query<(Entity, &Position), (With<Character>, Without<Player>)>,
+    mut target_events: EventReader<TargetEvent>,
+) {
+    let (vs, mut target) = player_query.single_mut();
+
+    let mut targets: Vec<_> = targets_query
+        .iter()
+        .filter_map(|(id, pos)| vs.includes(pos).then_some(id))
+        .collect();
+
+    targets.sort_unstable();
+
+    for event in target_events.iter() {
+        let new_target_id = match event {
+            TargetEvent::Prev => (target.0)
+                .and_then(|curr| {
+                    targets
+                        .windows(2)
+                        .find_map(|t| (t[1] == curr).then_some(t[0]))
+                })
+                .or_else(|| targets.last().copied()),
+
+            TargetEvent::Next => (target.0)
+                .and_then(|curr| {
+                    targets
+                        .windows(2)
+                        .find_map(|t| (t[0] == curr).then_some(t[1]))
+                })
+                .or_else(|| targets.first().copied()),
+
+            TargetEvent::Set(target_id) => targets.iter().find(|&id| id == target_id).copied(),
+        };
+
+        debug!("targeting {new_target_id:?}");
+        target.0 = new_target_id;
     }
 }
